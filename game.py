52f4928153918d92e6f8292cd8474ced6d2764ee
shemiramoth <mira.mugo@gmail.com>
2023-02-16 10:24:54 +0300

    lesson3

--- game.py
+++ game.py
@@ -7,10 +7,27 @@ print("Hello "+ username + "!")
 
 
 #2. generate a random number
-number= randint(10,50)
+random_number= randint(10,50)
  
 #3. using a while loop, check if user input is equal to generated number
 counter = 0
 while counter < 5:
     user_number = eval(input("Enter a random number to win: "))
+    counter = counter+1 #also conter += 1
+
+
+    if user_number<random_number:
+        print("Your guess is too low, try again: ")
+    elif user_number> random_number:
+        print("Your guess is too high try agan: ")
+    elif user_number==random_number:
+        break
+
+
+
+if user_number == random_number:
+    print("Congratulations You Have WON!!")
+else:
+    print("Are you dumb..Game Over!! The correct number is ")
+    print(random_number)
 
